82d9f50dc5da3e19486ebf6264725f051cbf895a
eugene <kolapo.eugene@gmail.com>
2023-01-19 16:14:46 +0100

    wromg display vue config fix

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  /// fix the problem of css display disfferent in production and development mode
+    css: {
+        extract: false
+    },
+  configureWebpack: {
+    optimization: {
+      splitChunks: false  // fix the problem of css display disfferent in production and development mode
+    }
+  }
+
+
 })
