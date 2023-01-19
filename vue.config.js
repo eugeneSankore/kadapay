2fd30a826ecc54778bfbcbd781f2639f2d22cab0
eugene <kolapo.eugene@gmail.com>
2023-01-19 13:49:50 +0100

    re added extract css

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-   extractCSS: true,
+    css: {
+        extract: true
+    }
 
 })
